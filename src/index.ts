dab93d19dfb769c915b609f7b5d9cc384744380f
VolhaYu <olenka_glushko1993@mail.ru>
2022-12-18 12:19:57 +0300

    feat: add products card

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./css/style.css";
 import "./filtres";
 import "./products";
 import "./css/storePage.css";
+import "./test";
--- src/products.ts
+++ src/products.ts
@@ -34,3 +34,39 @@ const foundProduct = document.createElement("p");
 foundProduct.classList.add("found-product");
 foundProduct.textContent = "Found:100";
 sortProducts.appendChild(foundProduct);
+
+const cardsProducts = document.createElement("div");
+cardsProducts.classList.add("cards-products");
+divProducts.appendChild(cardsProducts);
+for (let i = 0; i <= 100; i++) {
+  const cardProduct = document.createElement("div");
+  cardProduct.classList.add("card-product");
+  cardsProducts.appendChild(cardProduct);
+  const productImg = document.createElement("div");
+  productImg.classList.add("product__img");
+  cardProduct.appendChild(productImg);
+  const productDescription = document.createElement("div");
+  productDescription.classList.add("product__description");
+  cardProduct.appendChild(productDescription);
+
+  const img = document.createElement("img");
+  img.classList.add("img");
+  productImg.append(img);
+
+  const titleProduct = document.createElement("h3");
+  titleProduct.classList.add("product__title");
+  titleProduct.textContent = " Title";
+  productDescription.append(titleProduct);
+
+  const wrapPriceButton = document.createElement("div");
+  wrapPriceButton.classList.add("wrap-price-button");
+  productDescription.append(wrapPriceButton);
+  const priceProduct = document.createElement("p");
+  priceProduct.classList.add("product__price");
+  priceProduct.textContent = "Price";
+  wrapPriceButton.append(priceProduct);
+  const buttonToCart = document.createElement("button");
+  buttonToCart.classList.add("button-to-cart");
+  buttonToCart.textContent = "add to cart";
+  wrapPriceButton.append(buttonToCart);
+}
